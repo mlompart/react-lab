42570269ee5560ed7a233d8e1df9097f2079f8b2
mlompart <mateuszlompart@gmail.com>
2025-01-12 14:59:34 +0100

    Added movies map

--- src/App.js
+++ src/App.js
@@ -6,27 +6,40 @@ function App() {
   const [title, setTitle] = useState('Wall-E');
 
   function handleChange(event) {
-      setTitle(event.target.value);
+    setTitle(event.target.value);
   }
+
   let messege;
-  if(title.length < 5)
-  {
-    messege = "tytuł jest za krótki";
+  if (title.length < 5) {
+    messege = "Tytuł filmu jest za krótki";
   }
-  else if(title.length < 15)
-  {
-    messege= "tytuł jest extra";
+  else if (title.length < 15) {
+    messege = "Tytuł filmu jest extra!";
   }
-  else{
-    messege = "tytuł jest za długi";
+  else {
+    messege = "Tytuł filmu jest za długi";
   }
+
+  const movies = [
+    { title: "Wall-E" },
+    { title: "Pulp Fiction" },
+    { title: "Matrix" },
+    { title: "1670" },
+  ];
 
   return (
     <div>
-      <h1>My favourite movies for today</h1>
+      <h1>My favourite movies to watch</h1>
+      <h2>Titles</h2>
+      <ul>
+        {movies.map((movie) => <li key={movie.title}>{movie.title}</li>)}
+      </ul>
       <h2>My favourite movies for today is {title}</h2>
       {title.length > 0 && <div>{messege}</div>}
-      <input type="text" value={title} onChange={handleChange}/>
+      <input type="text" value={title} onChange={handleChange} />
+      <button type="button" onClick={() => alert(title)}>
+        Pokaż tytuł filmu
+      </button>
     </div>
   );
 }
